17eff8f2b86831e3c10af580ba4e2f5b868e4c78
camacleod527 <carolyn@melaniecasey.com>
2021-02-15 08:47:03 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App">
-    Weather App
+  return (
+  <div className="App">
+    <div className="container">
+   <h1>Weather App
+     </h1>
     <footer>
       This project was coded by Carolyn MacLeod and is <a href="https://github.com/camacleod527/react-final-project" 
   target="_blank">
     open-sourced on GitHub
   </a>
   </footer>
-  </div>;
+  </div>
+  </div>
+  );
 }
