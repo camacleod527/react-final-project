c4fdbade94b608fee197d134525fb6d154aae595
camacleod527 <carolyn@melaniecasey.com>
2021-02-16 08:38:51 -0500

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -6,13 +6,17 @@ export default function Weather() {
     <div className="Weather">
         <form>
             <div className="row">
-                <div className="col-9">
+            <div className="col-9">
             <input type="search" placeholder="Enter a city..." 
             className="form-control"
+            autoFocus="on"
             />
             </div>
             <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input type="submit" 
+            value="Search" 
+            className="btn btn-primary w-100" 
+            />
             </div>
             </div>
         </form>
@@ -27,12 +31,18 @@ export default function Weather() {
         Mostly cloudy
     </li>
 </ul>
-<div className="row">
+<div className="row mt-3">
     <div className="col-6">
+        <div className="clearfix">
         <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
         alt="Mostly Cloudy"
+        className="float-left"
         />
-        6 C
+        <div className="float-left">
+        <span className="temperature">6</span>
+        <span className="unit">C</span>
+        </div>
+        </div>
     </div>
     <div className="col-6">
         <ul>
