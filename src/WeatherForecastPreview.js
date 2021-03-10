c18f06a0e7e0cbcb54f38bac895c0b62ac669830
camacleod527 <carolyn@melaniecasey.com>
2021-03-10 14:23:13 -0500

    fixed forecast preview

--- src/WeatherForecastPreview.js
+++ src/WeatherForecastPreview.js
@@ -1,24 +1,25 @@
+
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
-export default function WeatherForecastPreview(props){
-    function hours() {
-        let date = new Date(props.data.dt * 1000);
-        let hours = date.getHours();
-        return `${hours}:00`;
-    }
+export default function WeatherForecastPreview(props) {
+  function hours() {
+    let date = new Date(props.data.dt * 1000);
+    let hours = date.getHours();
+    return `${hours}:00`;
+  }
 
-    function temperature() {
-        let temperature = Math.round(props.data.main.temp);
+  function temperature() {
+    let temperature = Math.round(props.data.main.temp);
 
-        return `${temperature}°C`;
-    }
+    return `${temperature}°C`;
+  }
 
-    return (
-    <div classname="WeatherForecastPreview col">
+  return (
+    <div className="WeatherForecastPreview col">
       {hours()}
-            <WeatherIcon code={props.data.weather[0].icon} />
-        {temperature()}
-        </div>
-        );
+      <WeatherIcon code={props.data.weather[0].icon} />
+      {temperature()}
+    </div>
+  );
 }
